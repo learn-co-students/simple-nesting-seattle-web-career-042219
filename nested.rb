5247994807c9f5ee7d033c93147206cd871053f7
Jennifer Kim <jjkim429@gmail.com>
2019-04-12 00:46:43 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-     return programmer_hash[:grace_hopper]
-
+     
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -35,7 +35,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     return programmer_hash[:alan_kay][:known_for]
+     
+  #solution below   
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +57,8 @@ def dennis_ritchies_language
         }
      }
      
-     return programmer_hash[:dennis_ritchie][:language]
+  #solution below   
+  programmer_hash[:dennis_ritchie][:languages].first
 end
 
 def adding_matz
@@ -74,8 +77,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
+     
+  #solution below
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash
 end
 
 def changing_alan
@@ -95,9 +100,11 @@ def changing_alan
         }
      }
    
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+      alans_new_info = "GUI"
+      
+  #solution below
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -117,7 +124,7 @@ def adding_to_dennis
         }
      }
   
-  programmer_hash[:dennis_ritchie][:languages] => ["C", "Assembley"]
-  return programmer_hash[:dennis_ritchie][:languages]
-
+  #solution below
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
